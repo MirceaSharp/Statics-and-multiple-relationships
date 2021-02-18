79980447c78676f72a37a7d4b8639281e0d099e0
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-02-18 15:51:23 +0100

    Initial2

---
+++ Statics and multiple relationships/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statics_and_multiple_relationships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            fdddddddddddddddd
+
+        }
+    }
+}
